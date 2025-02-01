3c815cd204c266e302106a559db8dd8ab00424fe
FDelporte <FDelporte@users.noreply.github.com>
2025-02-01 14:51:27 +0000

    deploy: 673de6c45248a6b589221dcfa4748f8b3b1df7de

---
+++ searchindex.en.js
@@ -0,0 +1,50 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Welcome to JTaccuino",
+    "content": "Usage Instructions",
+    "description": "Usage Instructions",
+    "tags": [],
+    "title": "How to Use",
+    "uri": "/use/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Welcome to JTaccuino JTaccuino is a JavaFX based notebook application for Java developers.\nIt is built for usages in education, interactive experimentation with algorithms, and more advanced use cases.\nJava code execution is provided by JShell, the awesome Java REPL.",
+    "description": "Welcome to JTaccuino JTaccuino is a JavaFX based notebook application for Java developers.\nIt is built for usages in education, interactive experimentation with algorithms, and more advanced use cases.\nJava code execution is provided by JShell, the awesome Java REPL.",
+    "tags": [],
+    "title": "Welcome to JTaccuino",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Welcome to JTaccuino",
+    "content": "Download the Application Builds for Windows, Linux, and macOS are available on the releases page of the GitHub project.\nExamples You can find many examples in the GitHub repository.\nDownload the Sources In case you want to learn how the application works, file a bug, propose an improvement, please check the GitHub repository",
+    "description": "Download the Application Builds for Windows, Linux, and macOS are available on the releases page of the GitHub project.\nExamples You can find many examples in the GitHub repository.\nDownload the Sources In case you want to learn how the application works, file a bug, propose an improvement, please check the GitHub repository",
+    "tags": [],
+    "title": "Download and Sources",
+    "uri": "/download/index.html"
+  },
+  {
+    "breadcrumb": "Welcome to JTaccuino",
+    "content": "Learn more about JTaccuino The project got presented at various conferences.\nDevoxx 2024, Belgium JChampions Conference 2025, Online",
+    "description": "Learn more about JTaccuino The project got presented at various conferences.\nDevoxx 2024, Belgium JChampions Conference 2025, Online",
+    "tags": [],
+    "title": "Videos",
+    "uri": "/videos/index.html"
+  },
+  {
+    "breadcrumb": "Welcome to JTaccuino",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Welcome to JTaccuino",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
